183a51bc5cd0388ff237b42a4bd398bf8c04243c
JB Bal <bal@abvent.fr>
2024-03-06 16:11:01 +0100

    ✨ Create missing views

---
+++ src/components/views/SkillsView/SkillsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span>Compétences</span>
+</template>
+
+<style lang="scss">
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@views/HomeView/HomeView.vue'
 import ReferencesView from '@views/ReferencesView/ReferencesView.vue'
+import SkillsView from '@views/SkillsView/SkillsView.vue'
+import ContactView from '@views/ContactView/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/references',
       name: 'references',
       component: ReferencesView
+    },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: SkillsView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
     }
   ]
 })
