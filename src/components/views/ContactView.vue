37785067a292cd495f13ca4a0b5a8678de4cfeea
JB Bal <bal@abvent.fr>
2024-03-15 11:20:57 +0100

    ✨ Create the map

--- src/components/views/ContactView.vue
+++ src/components/views/ContactView.vue
@@ -1,39 +1,39 @@
 <script setup lang="ts">
+import ContactMap from './childs/Contact/ContactMap.vue'
 </script>
 
 <template>
   <section class="contact-section">
-    <div class="contact-section__cover contact-section__cover--blur" />
-    <div class="contact-section__cover contact-section__cover--color" />
-    <article class="contact__card">
-      <span>Hello world!</span>
+    <div class="contact-section__cover" />
+    <article class="contact-section__card">
+      <contact-map />
     </article>
   </section>
 </template>
 
 <style lang="scss">
 .contact-section {
+  align-items: center;
   background: center / cover no-repeat url("/backgrounds/contact.jpeg");
   height: 100dvh;
+  display: flex;
+  justify-content: center;
   position: relative;
   width: 100%;
 
   &__cover {
+    background-color: var(--cover-soft);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
     height: 100%;
     left: 0;
     position: absolute;
     width: 100%;
+  }
 
-    // Duplicating the layers avoid having blurry edges at certain sizes
-
-    &--color {
-      background-color: var(--cover-soft);
-    }
-
-    &--blur {
-      -webkit-backdrop-filter: blur(10px);
-      backdrop-filter: blur(10px);
-    }
+  &__card {
+    display: flex;
+    position: relative;
   }
 }
 </style>
