560e7e3d5c58f3914ef1e2d29fe0cef087cc4c32
JB Bal <bal@abvent.fr>
2024-03-21 17:33:33 +0100

    ⏪ Revert the last commit

--- src/components/views/childs/Reference/ReferenceCarousel.vue
+++ src/components/views/childs/Reference/ReferenceCarousel.vue
@@ -51,6 +51,22 @@ onMounted(() => {
     else if (pointer.end - pointer.start > 100) toPrevSlide()
   }, true)
 })
+
+
+// Prevent the scroll while swiping
+const touch = {
+  start: 0,
+  end: 0
+}
+
+carousel.value?.addEventListener('touchstart', event => {
+  touch.start = event.touches[0].clientY
+})
+
+carousel.value?.addEventListener('touchmove', event => {
+  touch.end = event.touches[0].clientY
+  if (Math.abs(touch.end - touch.start) < 10) event.preventDefault()
+})
 </script>
 
 <template>
@@ -82,8 +98,8 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   margin-bottom: 56px;
-  // -webkit-user-select: none;
-  // user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
   width: 100%;
 
   &__carousel {
